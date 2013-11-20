d1db67af2df1053dbd72f058e9c904bf1d842ec3
Dane Lipscombe <dane@lipscombe.com.au>
2013-11-20 16:30:38 +0900

    Restoring restart task

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,6 +55,14 @@ namespace :deploy do
     end
   end
 
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
   after :updated, "deploy:compile_assets"
   after :updated, "deploy:migrate"
   after :finishing, 'deploy:cleanup'
